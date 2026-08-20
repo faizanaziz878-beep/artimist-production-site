d94f06e6adeaad043adcfd47290234a12e48f6fa
faizanaziz878-beep <faizanaziz878@gmail.com>
2026-08-20 16:12:03 +0500

    SEO: explicit crawl rules for Googlebot, Bingbot, OAI-SearchBot

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,20 @@
 import type { MetadataRoute } from 'next'
 
+const BASE = 'https://www.artimistproductions.com'
+const OPEN = { allow: '/', disallow: ['/admin', '/api/'] }
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/admin', '/api/'],
-      },
+      { userAgent: '*', ...OPEN },
+      { userAgent: 'Googlebot', ...OPEN },
+      { userAgent: 'Googlebot-Image', ...OPEN },
+      { userAgent: 'Bingbot', ...OPEN },
+      { userAgent: 'OAI-SearchBot', ...OPEN },
+      { userAgent: 'ChatGPT-User', ...OPEN },
+      { userAgent: 'PerplexityBot', ...OPEN },
     ],
-    sitemap: 'https://www.artimistproductions.com/sitemap.xml',
-    host: 'https://www.artimistproductions.com',
+    sitemap: `${BASE}/sitemap.xml`,
+    host: BASE,
   }
 }
