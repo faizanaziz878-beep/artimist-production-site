ffe0186f08972134076e1afc5c2730137b7be17d
faizanaziz878-beep <faizanaziz878@gmail.com>
2026-08-31 16:58:28 +0500

    Add garage conversion SEO landing page

---
+++ app/garage-conversion-plans/page.tsx
@@ -0,0 +1,4 @@
+import { ConsumerSeoPage, consumerMetadata } from "../consumer-seo-page";
+import "../[landing]/premium-pages.css";
+export const metadata=consumerMetadata("garage-conversion-plans");
+export default function Page(){return <ConsumerSeoPage slug="garage-conversion-plans"/>;}
