6d8662c08c1af187af4565df58a0ad115f5780a5
faizanaziz878-beep <faizanaziz878@gmail.com>
2026-08-31 16:58:48 +0500

    Add house extension SEO landing page

---
+++ app/house-extension-plans/page.tsx
@@ -0,0 +1,4 @@
+import { ConsumerSeoPage, consumerMetadata } from "../consumer-seo-page";
+import "../[landing]/premium-pages.css";
+export const metadata=consumerMetadata("house-extension-plans");
+export default function Page(){return <ConsumerSeoPage slug="house-extension-plans"/>;}
